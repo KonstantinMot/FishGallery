f053eb8d56886c85fa096a01ad7ed2b274697926
Konstantin <motylev.k@gmail.com>
2024-10-05 19:48:02 +0300

    Init commit

--- app/src/main/java/com/bluerose/fishgallery/ui/theme/Type.kt
+++ app/src/main/java/com/bluerose/fishgallery/ui/theme/Type.kt
@@ -2,19 +2,29 @@ package com.bluerose.fishgallery.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.bluerose.fishgallery.R
 
+
+val fonts = FontFamily(
+    Font(resId = R.font.roboto_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.roboto_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.roboto_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.roboto_light, weight = FontWeight.Light)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
